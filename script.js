369633cd7b5df3439488487da0380de2169360b4
GustavoGPreti <gustavogpreti@gmail.com>
2025-02-20 11:06:05 -0300

    toggle theme button

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 document.querySelectorAll(".value").forEach(button => {
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
         document.querySelectorAll(".value").forEach(btn => btn.classList.remove("active"));
         this.classList.add("active");
     });
+});
+let temaAtual;
+const checkbox = document.querySelector(".checkbox");
+
+checkbox.addEventListener("change", () => {
+    temaAtual = document.body.classList.contains("dark") ? "dark" : "light";
+    document.body.classList.toggle("dark");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (checkbox.checked) {
+        document.body.classList.add("dark");
+    } else {
+        document.body.classList.add("light");
+    }
+
+    temaAtual = document.body.classList.contains("dark") ? "dark" : "light";
 });
